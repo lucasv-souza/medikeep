5f7b3274016044ca8e6fd51551fde7fc2177145b
Lucas V.S <lucas.viniciuss@outlook.com>
2026-04-12 10:25:24 -0300

    Simplify menu prompts and options in main.py

--- src/main.py
+++ src/main.py
@@ -3,30 +3,21 @@ from tracker import adicionar_medicamento, listar_medicamentos
 
 def menu():
     meus_remedios = []
-
     while True:
-        print("\n--- MediKeep: Controle de Medicamentos ---")
+        print("\n--- MediKeep: Controle ---")
         print("1. Adicionar Medicamento")
-        print("2. Listar Meus Medicamentos")
+        print("2. Listar")
         print("3. Sair")
-
-        opcao = input("Escolha uma opção: ")
-
+        opcao = input("Escolha: ")
         if opcao == "1":
-            nome = input("Nome do remédio: ")
-            hora = input("Horário (ex: 08:00): ")
+            nome = input("Nome: ")
+            hora = input("Hora: ")
             sucesso, msg = adicionar_medicamento(meus_remedios, nome, hora)
             print(msg)
-
         elif opcao == "2":
-            print("\nSua Lista:")
             print(listar_medicamentos(meus_remedios))
-
         elif opcao == "3":
-            print("Encerrando... Cuide da sua saúde!")
             break
-        else:
-            print("Opção inválida.")
 
 
 if __name__ == "__main__":
